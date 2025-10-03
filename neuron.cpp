c2d4627b709a1d3d41f66c49930197551591a458
soumilbaldota <ssb2234@columbia.edu>
2025-10-02 20:23:25 -0400

    neuron calc using loop

--- neuron.cpp
+++ neuron.cpp
@@ -1,16 +1,14 @@
 int main() {
-    vector<double> weights1{1,2,3};
-    vector<double> weights2{1,2.5,3.5};
-    vector<double> weights3{4,1,2.5};
-
+    vector<vector<double>> weights{{1,2,3}, {1,2.5,3.5}, {4,1,2.5}};
     vector<double> input{1,2,3};
+    double bias = {3,2,1};
 
-    double bias1 = 1;
-    double bias2 = 2;
-    double bias3 = 3;
-
-    double res = (weights1[0] * input[0] + weights1[1] * input[1] + weights1[2] * input[2]) + bias1 +
-                (weights2[0] * input[0] + weights2[1] * input[1] + weights2[2] * input[2]) + bias2 +
-                (weights3[0] * input[0] + weights3[1] * input[1] + weights3[2] * input[2]) + bias3;
+    for(int _w = 0; _w < weights.size(); _w++) {
+        double res = bias[_w];
+        for(int _i = 0; _i < input.size(); _i++) {
+            res += input[_i] * weights[_w][_i];
+        }
+    }
+    
     return res;
 }
